579e489ec2e8e3530c32c6652f59314db42bc7b4
Oleksii Mytko <mytko.oleksii@gmail.com>
2023-12-14 02:22:07 +0200

    add jest test ant supertest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
 <<<<<<< HEAD
+<<<<<<< HEAD
     preset: 'ts-jest',
     testEnvironment: 'node',
 =======
   preset: 'ts-jest',
   testEnvironment: 'node',
 >>>>>>> 4189879 (add jest test ant supertest)
+=======
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+>>>>>>> ee3ee7b (add jest test ant supertest)
 };

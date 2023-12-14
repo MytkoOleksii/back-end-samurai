63a10a1ecb5a1aa7b458160d8394b281ea030359
Oleksii Mytko <mytko.oleksii@gmail.com>
2023-12-14 21:26:40 +0200

    1

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,14 @@
 module.exports = {
 <<<<<<< HEAD
 <<<<<<< HEAD
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+=======
     preset: 'ts-jest',
     testEnvironment: 'node',
+>>>>>>> 13_swagger_OpenAPI222222
 =======
   preset: 'ts-jest',
   testEnvironment: 'node',
->>>>>>> 4189879 (add jest test ant supertest)
-=======
-  preset: 'ts-jest',
-  testEnvironment: 'node',
->>>>>>> ee3ee7b (add jest test ant supertest)
+>>>>>>> origin/main
 };

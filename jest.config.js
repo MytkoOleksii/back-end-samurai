bb4682ec2fd1d5559ce88fe405c6b1d7a0a79b62
Oleksii Mytko <mytko.oleksii@gmail.com>
2023-12-14 21:26:40 +0200

    1

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
   preset: 'ts-jest',
   testEnvironment: 'node',
-=======
-    preset: 'ts-jest',
-    testEnvironment: 'node',
->>>>>>> 13_swagger_OpenAPI222222
-=======
-  preset: 'ts-jest',
-  testEnvironment: 'node',
->>>>>>> origin/main
 };
